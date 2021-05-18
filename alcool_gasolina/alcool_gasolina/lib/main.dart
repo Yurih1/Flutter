b82110c48c2c57f3be437c7589e16d6562cbe277
yurih1 <yuri.silva15@live.com>
2021-05-18 20:42:10 -0300

    First application

---
+++ alcool_gasolina/alcool_gasolina/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: Home()));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:
+            Text("Álcool ou Gasolina", style: TextStyle(color: Colors.white)),
+        centerTitle: true,
+        backgroundColor: Colors.lightBlue[900],
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.refresh), onPressed: () {})
+        ],
+      ),
+    );
+  }
+}
